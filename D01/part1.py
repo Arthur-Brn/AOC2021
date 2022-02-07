d444687ccf594a5b35bf76e5014e045e08588423
ArthurB.Dev <>
2022-02-07 11:57:57 +0100

    Simplify the code of the day 1

--- D01/part1.py
+++ D01/part1.py
@@ -1,7 +1,8 @@
-
 def run(list_of_number):
-    list_of_change = []
-    for index in range(len(list_of_number) - 1):
-        list_of_change.append(1 if list_of_number[index] < list_of_number[index + 1] else 0)
+    return sum([1 if x[0] < x[1] else 0
+                for x in (
+                    [list_of_number[index:index + 2]
+                     for index in (range(len(list_of_number) - 1))]
+                )]
+               )
 
-    return sum(list_of_change)
--- D01/part2.py
+++ D01/part2.py
@@ -1,7 +1,5 @@
-
 def run(list_of_number):
-    list_of_number_linear = []
-    for index in range(len(list_of_number) - 2):
-        list_of_number_linear.append(list_of_number[index] + list_of_number[index + 1] + list_of_number[index + 2])
-    print(list_of_number_linear)
-    return list_of_number_linear
+    return [sum(x) for x in (
+        [list_of_number[index:index + 3]
+         for index in range(len(list_of_number) - 2)
+         ])]
