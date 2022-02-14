1ed80666e453a0c3ee40475e1dc562f2bdac8580
ArthurB.Dev <>
2022-02-15 00:35:36 +0100

    fix some problems in main
    Translate in english
    Clear after answer
    Add message

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def showMainMenu():
     run = 1
 
     while run:
-        print("1- Voir les solutions")
-        print("2 - Voir les crédits")
-        print("q - Sortir du programme")
-        choose = input("Saissisez votre réponse").strip()
+        print("1 - See solutions of every day")
+        print("2 - See credits")
+        print("q - Exit the program")
+        choose = input("Enter a choose : ").strip()
         if choose == "1":
             runADay()
         elif choose == "2":
@@ -34,24 +34,27 @@ def runADay():
             if re.match('^(D)[0-2][0-9]$', ele):
                 list_folder.append(ele)
 
+    list_folder.sort()
     unvalid_choose = 1
 
     while unvalid_choose:
         [print(f'{i + 1}) {x}') for i, x in enumerate(list_folder)]
-        choosed_day = input("Veuillez selectionner un jour (1,2...) : ").strip()
+        choosed_day = input("Select a day (1,2...) : ").strip()
         if choosed_day.isdigit() and list_folder[int(choosed_day) - 1]:
             day = list_folder[int(choosed_day) - 1]
             openDay(day)
             unvalid_choose = 0
         else:
-            print(f'Le nombre {choosed_day} n\'existe pas')
+            print(f'The day {choosed_day} doesn\'t exist')
 
 
 def openDay(path_folder):
     gbl[path_folder] = importlib.import_module(f'{path_folder}.{path_folder}')
     day = getattr(globals()[path_folder], path_folder)()
-    print(day.part1())
-    print(day.part2())
+    print(f"Part 1 answer : {day.part1()}")
+    print(f"Part 2 answer : {day.part2()}")
+    input("Press any key to continue...")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 if __name__ == '__main__':
